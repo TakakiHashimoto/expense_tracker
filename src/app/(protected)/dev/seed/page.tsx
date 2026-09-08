b7d7f459ec684d208a62f01678a4c01e1dcc3047
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-09-08 00:12:56 -0700

    feat: add environment check to prevent seeding data in production

--- src/app/(protected)/dev/seed/page.tsx
+++ src/app/(protected)/dev/seed/page.tsx
@@ -1,6 +1,11 @@
 import { seedData } from "@/features/dev/seed/action";
+import { notFound } from "next/navigation";
 
 export default function SeedData() {
+  if (process.env.NODE_ENV !== "development") {
+    notFound();
+  }
+
   return (
     <div className="flex justify-center items-center">
       <form action={seedData} className="my-6">
