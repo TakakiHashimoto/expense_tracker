eba005e70157b4082482aa8bee764e935de5a7a8
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-09-11 06:40:01 -0700

    feat: update construction message with emoji for better visibility

--- src/app/(protected)/subscription/page.tsx
+++ src/app/(protected)/subscription/page.tsx
@@ -2,7 +2,7 @@ function page() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center ">
       <p className="text-4xl font-bold">
-        This feature is under construction...{" "}
+        This feature is under construction...👷‍♂️
       </p>
       <p>Stay tuned!</p>
     </div>
